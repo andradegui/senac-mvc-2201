d09508bcb050ced7b97378baf4ff00dd1171d9e8
Guilherme <guirafaellima@outlook.com>
2022-03-11 21:26:43 -0300

    Aula 11-03-22

--- primeiro-site/routes/web.php
+++ primeiro-site/routes/web.php
@@ -2,17 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+//método get -> oferece a alguém fazer o get via browser
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/avisos', function () {
+    return view('avisos', ['nome'=> 'Guilherme',
+                                    'mostrar'=>true,
+                                    'avisos' => [   ['id' => 1, 'aviso' => 'Mussum Ipsum, cacilds vidis litro abertis.'],
+                                                    ['id' => 2, 'aviso' => 'Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo!.'],
+                                                    ['id' => 3, 'aviso' => 'Quem num gosta di mim que vai caçá sua turmis!.']
+                                    ]]);
 });
