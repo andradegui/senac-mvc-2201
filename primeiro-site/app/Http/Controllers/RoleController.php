d133eafcc9f1afe4943b9fef3658ff981ce6cb34
Guilherme <guirafaellima@outlook.com>
2022-03-25 22:18:34 -0300

    Aula Finalizada

--- primeiro-site/app/Http/Controllers/RoleController.php
+++ primeiro-site/app/Http/Controllers/RoleController.php
@@ -33,7 +33,9 @@ class RoleController extends Controller
      */
     public function index()
     {
-        //
+        $roles = Role::orderBy('id', 'DESC')->paginate(5);
+
+        return view('roles.index', compact('roles'))->with('i', ($request->input('page', 1) -1) *5);
     }
 
     /**
@@ -43,7 +45,9 @@ class RoleController extends Controller
      */
     public function create()
     {
-        //
+        $permissions = Permission::get();
+
+        return view('roles.create', compact('permission'));
     }
 
     /**
@@ -54,7 +58,14 @@ class RoleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [ 'name' => 'required|unique:roles,name',
+                                    'permission' => 'required']); //validando dados passados (qual permissão?)
+
+        $role = Role::create(['name' => $request->input('name')]);
+
+        $role->syncPermissions($request->input('permission'));
+
+        return redirect()->route('roles.index')->with('success', 'Perfil criado com sucesso');
     }
 
     /**
@@ -65,7 +76,14 @@ class RoleController extends Controller
      */
     public function show($id)
     {
-        //
+        $role = Role::find($id);
+
+        $rolePermissions = Permission::join(    'role_has_permissions',
+                                                'role_has_permissions.permissions_id',
+                                                '=',
+                                                'permissions_id')->where('role_has_permission.role_id', $id)->get();
+
+        return view('roles.show', compact('role', 'rolePermissions'));
     }
 
     /**
